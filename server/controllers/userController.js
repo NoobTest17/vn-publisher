5bb269e55cb828c7cdd73a65bad9773c44d7d7f1
NoobTest17 <maximnnn.nnn@yandex.ru>
2023-07-06 21:34:42 +0300

    start dev

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+class UserController {
+  async registation(req, res, next) {
+
+  }
+
+  async login(req, res, next) {
+    
+  }
+  
+  async check(req, res, next) {
+
+  }
+}
+
+module.exports = new UserController();
